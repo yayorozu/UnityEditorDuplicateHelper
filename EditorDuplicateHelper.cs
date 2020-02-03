0e2197476b378150ef87cf9b748eae5c51ec19e8
hatch <hatch.tech.eng@gmail.com>
2020-02-03 23:47:55 +0900

    addTool

---
+++ EditorDuplicateHelper.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace UniLib
+{
+	public class EditorDuplicateHelper
+	{
+		[InitializeOnLoadMethod]
+		private static void SetDelegate()
+		{
+			EditorApplication.hierarchyWindowItemOnGUI += OnHierarchyWindowItemOnGUI;
+		}
+
+		private static void OnHierarchyWindowItemOnGUI(int instanceID, Rect rect)
+		{
+			if (Selection.activeInstanceID != instanceID)
+				return;
+
+			var e = Event.current;
+
+			if (e.type != EventType.ValidateCommand && e.type != EventType.ExecuteCommand)
+				return;
+
+			if ("Duplicate" != e.commandName)
+				return;
+
+			var gameObject = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+			if (gameObject == null)
+				return;
+
+			var scale = gameObject.transform.localScale;
+			EditorApplication.delayCall += () =>
+			{
+				var activeGameObject = Selection.activeGameObject;
+				if (activeGameObject == null)
+					return;
+			
+				activeGameObject.transform.name = name;
+				activeGameObject.transform.localScale = scale;
+			};
+		}
+	}
+}
